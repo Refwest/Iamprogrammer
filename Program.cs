cd69be72cb4e9835c56bc55a3e2a940bf3566cd6
Filip Działa <fi.dziala@gmail.com>
2023-06-23 23:54:45 +0200

    Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -13,8 +13,16 @@ class Program
 
         int myIntegers2;
         myIntegers2 = 7;
-
         int Sum = myIntegers - myIntegers2;
+
+        double myDouble = 2.5;
+        string myString = "I am programmer";
+        bool myTrue = true;
+        bool myTrue2 = false;
+        char myCharacter = 'c';
+
+        Console.WriteLine("The sentence that {0} is {1,-5}.", myString, myTrue);
+        Console.WriteLine("My mark for programming is " + myIntegers);
 
 
     
