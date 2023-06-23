b01f7bcc6a715a1ba9e136e825b49d1d9c021b09
Filip Działa <fi.dziala@gmail.com>
2023-06-23 18:14:42 +0200

    First steps

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//See https://aka.ms/new-console-template for more information
+
+
+namespace Iamprogrammer;
+
+class Program
+{
+    //Pierwsze kroki w programowaniu językiem C#
+    static void Main(string[] args)
+    {
+        int myIntegers = 5;
+        myIntegers = 6;
+
+        int myIntegers2;
+        myIntegers2 = 7;
+
+        int Sum = myIntegers - myIntegers2;
+
+
+    
+    }
+}
